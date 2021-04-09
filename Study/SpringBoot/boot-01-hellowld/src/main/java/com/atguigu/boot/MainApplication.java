73f5aeef2c547a3f1c70de2863e54a2d658dd4b3
yangjie <touhoyang@gmail.com>
2021-04-09 15:14:57 +0800

    新建

---
+++ Study/SpringBoot/boot-01-hellowld/src/main/java/com/atguigu/boot/MainApplication.java
@@ -0,0 +1,23 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * 主程序类
+ * @SpringBootApplication:这是一个SpringBoot应用
+ */
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args){
+        //1.返回我们IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class,args);
+
+//        String[] names = run.getBeanDefinitionNames();
+//        for (String name : names) {
+//            System.out.println(name);
+//        }
+    }
+}
---
+++ Study/SpringBoot/boot-01-hellowld/src/main/java/com/atguigu/boot/controller/HelloContreller.java
@@ -0,0 +1,15 @@
+package com.atguigu.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/*@Controller @ResponseBody*/
+@RestController
+public class HelloContreller {
+    @RequestMapping("/hello")
+    public String hendle01() {
+        return "Hello,Spring Boot 2!";
+    }
+}

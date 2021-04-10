24e6773970ac5fa1b1df8c0a7bee6e59e50766a9
yangjie <touhoyang@gmail.com>
2021-04-10 16:55:43 +0800

    update

--- Study/SpringBoot/boot-01-hellowld/src/main/java/com/atguigu/boot/MainApplication.java
+++ Study/SpringBoot/boot-01-hellowld/src/main/java/com/atguigu/boot/MainApplication.java
@@ -1,23 +1,54 @@
 package com.atguigu.boot;
 
+import com.atguigu.boot.bean.Pet;
+import com.atguigu.boot.bean.User;
+import com.atguigu.boot.config.MyConfig;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.time.Period;
 
 /**
  * 主程序类
  * @SpringBootApplication:这是一个SpringBoot应用
  */
 
-@SpringBootApplication
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan("com.atguigu.boot")
 public class MainApplication {
     public static void main(String[] args){
         //1.返回我们IOC容器
         ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class,args);
 
-//        String[] names = run.getBeanDefinitionNames();
-//        for (String name : names) {
-//            System.out.println(name);
-//        }
+        //2.查看容器里面的组件
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        //3.从容器中获取组件
+
+        Pet tom01 = run.getBean("tom", Pet.class);
+        Pet tom02 = run.getBean("tom", Pet.class);
+        System.out.println("组件:" + (tom01 == tom02));
+
+        MyConfig bean = run.getBean(MyConfig.class);
+        System.out.println(bean);
+
+        //如果@Configurotion(proxyBeanMethods = true)代理对象调用方法。SpringBoot总会检查这个组件是否在容器中有，如果有就不会新建，包吃组件单实例。
+        User user = bean.user01();
+        User user02 = bean.user01();
+        System.out.println(user == user02);
+
+        User user01 = run.getBean("user01",User.class);
+        Pet tom = run.getBean("tom",Pet.class);
+
+        System.out.println ("用户的宠物：" + (user01.getPet() == tom));
+
     }
 }
---
+++ Study/SpringBoot/boot-01-hellowld/src/main/java/com/atguigu/boot/config/MyConfig.java
@@ -0,0 +1,37 @@
+package com.atguigu.boot.config;
+
+
+import com.atguigu.boot.bean.Pet;
+import com.atguigu.boot.bean.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 1.配置类里面使用@Bean标注在方法上给容器注册组件，默认也是单实例的
+ * 2.配置类本身也是组件
+ * 3.proxyBeanMethods:代理bean的方法
+ * Full(proxyBeanMethods = true)
+ * Lite(proxyBeanMethods = false)轻量级
+ * 组建
+ */
+@Configuration(proxyBeanMethods = true)//告诉SpringBoot这是一个配置类 == 配置文件
+public class MyConfig {
+
+    /**
+     * 外部无论对配置类中的这个组件注册方法调用多少次，获取的都是之前注册容器中的单实例对象
+     */
+
+    @Bean //给容器中添加组件，以方法名作为组件的id，返回类型就是组件类型，返回的值就是组件在容器中的实例。
+    public User user01() {
+        User zhangsan = new User("张三", 18);
+        //user组件依赖了Pet组件
+        zhangsan.setPet(tomcatPet());
+        return zhangsan;
+    }
+
+    @Bean("tom")//把组件名称重命名为tom
+    public Pet tomcatPet() {
+        return new Pet("tomcat");
+    }
+
+}

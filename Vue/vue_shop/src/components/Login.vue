95df8aaf4b9431dcec1c68524efc5e642d121cd1
yangjie <touhoyang@gmail.com>
2021-04-11 18:49:09 +0800

    update login

---
+++ Vue/vue_shop/src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login_container">
+    登录组件
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<!--
+lang = "less" 这个节点中支持less语法
+scoped 是一个vue的指令，用来控制组件的样式的生效的区间；加上scoped，只在当前组件的样式生效
+-->
+<style lang="less" scoped>
+.login_container{
+  background-color: #2b4b6b;
+}
+</style>

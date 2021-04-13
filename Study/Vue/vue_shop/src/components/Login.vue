d2290ee0c984a9ea09a42f765724cb23ab3fbfb9
yangjie <touhoyang@gmail.com>
2021-04-13 18:43:01 +0800

    更新登录页面代码

--- Study/Vue/vue_shop/src/components/Login.vue
+++ Study/Vue/vue_shop/src/components/Login.vue
@@ -2,15 +2,76 @@
   <div class="login_container">
     <!-- 登录组件 -->
     <div class= "login_box">
+      <!-- 头像区 -->
       <div class="avtar_box">
         <img src="../assets/logo.png" alt="">
       </div>
+      <!-- 表单区 -->
+      <el-form ref='loginFormRef' :rules="loginFormRules" :model="loginForm" label-width="0px" class="login_form">
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="iconfont icon-user"
+            v-model="loginForm.username">
+          </el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="iconfont icon-3702mima"
+            v-model="loginForm.password"
+            type="password">
+          </el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" round @click="login">登录</el-button>
+          <el-button type="info" round @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // 这是登录表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: 'admin'
+      },
+      // 表单验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 5, max: 10, message: '长度在 5 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 5, max: 15, message: '长度在 5 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮，点击重置表单
+    resetLoginForm () {
+      // 可以使用多个方法，不需要添加分号即可
+      console.log(this)
+      // 这里不用添加分号
+      this.$refs.loginFormRef.resetFields()
+    }
+  },
+  login () {
+    this.$refs.loginFormRef.validate((valid) => {
+      console.log(valid)
+    })
+  }
+}
 </script>
 
 <!--
@@ -19,13 +80,13 @@ scoped 是一个vue的指令，用来控制组件的样式的生效的区间；�
 -->
 <style lang="less" scoped>//注意：这里老师要求使用vue ui安装less—loader，但是我们现在安装的版本太高了会导致报错，所以我们卸载了，或者不装然后在下面终端输入npm install less-loader@5.0.0安装好了就可以了，后面的less可以正常在vue ui里面安装
 .login_container{
-  background-color: #2b4b6b;
+  background-color:#2b4b6b;
   height: 100%;
 }
 .login_box{
   width: 450px;
   height: 300px;
-  background-color: #fff;
+  background-color:#fff;
   // 圆角属性
   border-radius: 3px;
   // 绝对定位
@@ -43,6 +104,12 @@ scoped 是一个vue的指令，用来控制组件的样式的生效的区间；�
     border-radius: 50%;
     //给图片和边框加一个10px边距
     padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    transform: tans;
+    background-color: #fff;
     img{
       height: 100%;
       width: 100%;
@@ -51,4 +118,20 @@ scoped 是一个vue的指令，用来控制组件的样式的生效的区间；�
     }
   }
 }
+
+.login_form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0px 20px;
+  box-sizing: border-box;
+}
+
+.btns{
+  // display: flex设置为弹性布局，
+  // justify-content: flex-end项目位于容器的结尾。
+  display: flex;
+  justify-content: flex-end;
+}
+
 </style>
--- Study/Vue/vue_shop/src/plugins/element.js
+++ Study/Vue/vue_shop/src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+// 按照老师的写法这里会报错，imported multiple times  import/no-duplicates
+// 大致意思是重复地导入了信息，所以我们把这些数据整合起来成一条导入信息就可以了。
+import { Button, Form, FormItem, Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
